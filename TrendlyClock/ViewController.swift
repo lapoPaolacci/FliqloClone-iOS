9b736d0000ffd05149463f6f11e7f040f54f3cfb
Lapo Paolacci <lapo.paolacci@gmail.com>
2021-09-25 15:08:31 +0200

    Initial Commit

---
+++ TrendlyClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendlyClock
+//
+//  Created by Lapo Paolacci on 25/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
